9952fea74d0efdf0dd7db71c31f0d79684743111
issac <issac@ip-192-168-0-143.ap-northeast-2.compute.internal>
2023-02-15 16:45:31 +0900

    basic oauth server

---
+++ src/main/java/co/coinvestor/oauthserver/config/AuthServerConfig.java
@@ -0,0 +1,55 @@
+package co.coinvestor.oauthserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig
+        extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .secret("secret")
+                .redirectUris("http://localhost:9090/home")
+                .authorizedGrantTypes("authorization_code", "password", "refresh_token")
+                .scopes("a","b");
+    }
+
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        var service = new InMemoryClientDetailsService();
+//
+//        var cd = new BaseClientDetails();
+//        cd.setClientId("client");
+//        cd.setClientSecret("secret");
+//        cd.setScope(List.of("read"));
+//        cd.setAuthorizedGrantTypes(List.of("password"));
+//
+//        service.setClientDetailsStore(Map.of("client", cd));
+//
+//        clients.withClientDetails(service);
+//    }
+
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security.checkTokenAccess("isAuthenticated()");
+    }
+}
