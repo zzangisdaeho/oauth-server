7fef57e70b9fe1ab0ec030754bd2f5622b13b98d
kylianju <kylian.ju@bclabs.co.kr>
2023-05-08 15:37:30 +0900

    circle ci deploy prep

---
+++ src/main/java/co/coinvestor/oauthserver/controller/TestController.java
@@ -0,0 +1,15 @@
+package co.coinvestor.oauthserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/healthcheck")
+    public void healthCheck(){
+
+    }
+}
