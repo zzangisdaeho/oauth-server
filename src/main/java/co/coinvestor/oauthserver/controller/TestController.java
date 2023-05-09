2fc8d5657847f34582fb9af52791ae661310b2b0
issac <issac.kim@bclabs.co.kr>
2023-05-09 17:26:59 +0900

    wtf

--- src/main/java/co/coinvestor/oauthserver/controller/TestController.java
+++ src/main/java/co/coinvestor/oauthserver/controller/TestController.java
@@ -2,6 +2,7 @@ package co.coinvestor.oauthserver.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class TestController {
     @GetMapping("/healthcheck")
     public void healthCheck(){
 
+    }
+
+    @PostMapping("/post")
+    public String postCheck(){
+        return "POST";
     }
 }
